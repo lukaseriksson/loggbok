8dfdb13cb6a53f99d12a5e8a567c970225cfc9f4
lukas.eriksson <lukas.eriksson2@elev.ga.ntig.se>
2019-09-03 09:09:30 +0200

    text

--- src/menu.java
+++ src/menu.java
@@ -7,14 +7,14 @@ public class menu {
     }
 
     public static void main(String[] args) {
-        System.out.println("Example Program");
-        System.out.println("");
+        System.out.println("loggbok");
+        System.out.println("vad vill du göra i loggboken");
         System.out.println("Menu Options:");
-        System.out.println("1. ");
-        System.out.println("2. ");
-        System.out.println("3. ");
-        System.out.println("4. ");
-        System.out.println("5. ");
+        System.out.println("1. visa");
+        System.out.println("2. skriv ny");
+        System.out.println("3. redigera");
+        System.out.println("4. spara");
+        System.out.println("5. läsa in gammal");
         System.out.println("6. Exit");
         System.out.print("Please select an option from 1-6\r\n");
 
@@ -24,7 +24,7 @@ public class menu {
             int input = Integer.parseInt(br.readLine());
 
             if(input < 0 || input > 6) {
-                System.out.println("You have entered an invalid selection, please try again\r\n");
+                System.out.println("That's the wrong number\r\n");
             } else if(input == 6) {
                 System.out.println("You have quit the program\r\n");
                 System.exit(1);
